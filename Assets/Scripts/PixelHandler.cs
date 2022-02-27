31b1fea4fc128e9ccf0a5b7067d3ef7ba5071205
mohGhazala96 <mohamed.aboughazala@student.guc.edu.eg>
2022-02-27 16:36:20 +0100

    final modifications

--- Assets/Scripts/PixelHandler.cs
+++ Assets/Scripts/PixelHandler.cs
@@ -31,25 +31,34 @@ public class PixelHandler : MonoBehaviour
         }
         transform.position = targetPosition;
         canHit = false;
+        Destroy(GetComponent<Rigidbody>());
     }
 
     IEnumerator Loop()
     {
+        gameObject.AddComponent<Rigidbody>();
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<Rigidbody>().isKinematic = false;
-        if (ConfigurationsManager.Instance.force == Force.FallBack)
+        if (ConfigurationsManager.Instance.force == Force.Fall)
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, Random.Range(-1, -10)) * 10, ForceMode.Impulse);
-
+            Physics.gravity = new Vector3(0, -30.0F, 0);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, Random.Range(1, 3),0) * 15, ForceMode.Impulse);
+        }
+        else if (ConfigurationsManager.Instance.force == Force.FallBack)
+        {
+            Physics.gravity = new Vector3(0, -40.0F, 0);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, Random.Range(-1, -7)) * 8, ForceMode.Impulse);
         }
         else if (ConfigurationsManager.Instance.force == Force.FallFront)
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, Random.Range(1, 10)) * 10, ForceMode.Impulse);
+            Physics.gravity = new Vector3(0, -40.0F, 0);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, Random.Range(1, 2.5f)) * 5, ForceMode.Impulse);
 
         }
         else if (ConfigurationsManager.Instance.force == Force.RandomScatter)
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10)) * 10, ForceMode.Impulse);
+            Physics.gravity = new Vector3(0, -30.0F, 0);
+            GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-5, 6), Random.Range(1, 5), Random.Range(-5, 5)) * 10, ForceMode.Impulse);
 
         }
         yield return new WaitForSeconds(4);
